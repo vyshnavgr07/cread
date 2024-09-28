0a6d9d14d82c044db6490ab90e4933635853e150
vyshnavgr07 <vyshnavgr07@gmail.com>
2024-09-28 19:21:20 +0530

    update

--- client/src/api/axiosInterceptor.js
+++ client/src/api/axiosInterceptor.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4004", 
+//   baseURL: "http://localhost:4004", 
+baseURL:"https://machine-task-m10z.onrender.com"
 });
 
 api.interceptors.request.use(
